3c137aa1d8b3d20a87c49cdaa73d8e24e2f4d2fd
Nadine Rodriguez <inadinerod@gmail.com>
2022-11-10 12:56:48 -0500

    Add lesson 5 challenge 3

---
+++ l5_c3.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NameTag from "./NameTag.js";
+import "./styles.css";
+
+const App = () => (
+  <div className="App">
+    <h1>Name Tag Generator</h1>
+    <NameTag name="Sunflower" />
+    <NameTag name="Hibiscus" />
+    <NameTag name="Daisy" />
+    <NameTag name="Lilly" />
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
