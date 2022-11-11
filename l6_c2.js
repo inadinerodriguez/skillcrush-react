9f34ea964e8f67e643661716798e9c0b2dbf9723
Nadine Rodriguez <inadinerod@gmail.com>
2022-11-11 10:34:58 -0500

    Add lesson 6 challenge 2

---
+++ l6_c2.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NameTag from "./NameTag.js";
+import { names } from "./data.js";
+import "./styles.css";
+
+const renderNameTag = (name) => <NameTag name={name.person} key={name.id} />;
+
+const App = () => {
+  const nameTagElements = names.map(renderNameTag);
+  return (
+    <div className="App">
+      <h1>Name Tag Generator</h1>
+      {nameTagElements}
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
